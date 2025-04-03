09bc87e2870d4d1ddf263bd6e0490bb73ddd05df
ysk826 <ysuke826@gmail.com>
2025-04-03 23:50:59 +0900

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -15,16 +15,26 @@ func main() {
 	// CORSミドルウェアの設定
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000"},
-		AllowMethods:     []string{"GET"},
-		AllowHeaders:     []string{"Origin", "Content-Type"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
 		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true}))
+		AllowCredentials: true,
+	}))
 
-	// エンドポイントの定義
-	r.GET("/api/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello,燈哉!"})
-	})
+	// apiエンドポイントのグループ化
+	api := r.Group("/api")
+	{
+		api.GET("/hello", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Hello,燈哉!",
+			})
+		})
+	}
+
+	// 日記のエンドポイント
+	api.GET("/diary", getDiaryEntriesByDate)
+	api.POST("/diary", createDiaryEntry)
+	//api.PUT("/diary/:id", updateDiaryEntry)
 
 	// サーバーの起動（エラーハンドリングを追加）
 	log.Println("サーバーを開始します...")
